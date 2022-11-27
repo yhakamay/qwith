663b885cc40b1b911d8d2370c0f6f89c6cd3becb
Yusuke Hakamaya <yhakamay@student.42tokyo.jp>
2022-11-27 17:50:17 +0900

    move pages/new.tsx to pages/new/index.tsx

---
+++ pages/new/[roomId].tsx
@@ -0,0 +1,114 @@
+import {
+  Button,
+  Center,
+  Divider,
+  FormControl,
+  FormLabel,
+  Input,
+  Spinner,
+  VStack,
+} from "@chakra-ui/react";
+import { addDoc, collection, doc } from "firebase/firestore";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import QuizzesList from "../../components/molecules/quizzesList";
+import { db } from "../../firebaseConfig";
+import { ChoiceQuiz } from "../../types/choiceQuiz";
+
+export default function CreateQuizPage() {
+  const router = useRouter();
+  const { roomId } = router.query;
+  const [loading, setLoading] = useState(false);
+  const [question, setQuestion] = useState("");
+  const [option1, setOption1] = useState("");
+  const [option2, setOption2] = useState("");
+  const [option3, setOption3] = useState("");
+  const [option4, setOption4] = useState("");
+
+  if (!router.isReady) {
+    return (
+      <Center>
+        <Spinner />
+      </Center>
+    );
+  }
+
+  return (
+    <VStack>
+      <FormControl isRequired>
+        <FormLabel>Question</FormLabel>
+        <Input
+          value={question}
+          onChange={(e) => setQuestion(e.target.value)}
+        ></Input>
+      </FormControl>
+
+      <Divider />
+
+      <FormControl isRequired isDisabled={loading}>
+        <FormLabel>Option #1 (Correct Answer)</FormLabel>
+        <Input
+          value={option1}
+          onChange={(e) => setOption1(e.target.value)}
+        ></Input>
+      </FormControl>
+      <FormControl isRequired isDisabled={loading}>
+        <FormLabel>Option #2</FormLabel>
+        <Input
+          value={option2}
+          onChange={(e) => setOption2(e.target.value)}
+        ></Input>
+      </FormControl>
+      <FormControl isDisabled={loading}>
+        <FormLabel>Option #3</FormLabel>
+        <Input
+          value={option3}
+          onChange={(e) => setOption3(e.target.value)}
+        ></Input>
+      </FormControl>
+      <FormControl isDisabled={loading}>
+        <FormLabel>Option #4</FormLabel>
+        <Input
+          value={option4}
+          onChange={(e) => setOption4(e.target.value)}
+        ></Input>
+      </FormControl>
+      <Button
+        onClick={addQuiz}
+        isLoading={loading}
+        isDisabled={question === "" || option1 === "" || option2 === ""}
+        width="full"
+      >
+        Add
+      </Button>
+      <QuizzesList roomId={roomId as string} />
+    </VStack>
+  );
+
+  // Add quiz to the room in firestore
+  async function addQuiz() {
+    setLoading(true);
+
+    const roomsRef = collection(db, "rooms");
+    const roomRef = doc(roomsRef, roomId as string);
+    const quizzesRef = collection(roomRef, "quizzes");
+    const quiz: ChoiceQuiz = {
+      question: question,
+      options: [option1, option2, option3, option4],
+      answer: option1,
+    };
+
+    try {
+      await addDoc(quizzesRef, quiz);
+      setQuestion("");
+      setOption1("");
+      setOption2("");
+      setOption3("");
+      setOption4("");
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+}
---
+++ pages/new/index.tsx
@@ -0,0 +1,137 @@
+import { Button, Heading, Input, Textarea, VStack } from "@chakra-ui/react";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { addDoc, collection, getDocs, query, where } from "firebase/firestore";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, db } from "../../firebaseConfig";
+import { Room } from "../../types/room";
+
+const signIn = () => {
+  const provider = new GoogleAuthProvider();
+
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential?.accessToken;
+      const user = result.user;
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+};
+
+const signOut = () => {
+  auth.signOut();
+};
+
+export default function CreateRoomPage() {
+  const [user, loadingAuth, error] = useAuthState(auth);
+  const [loading, setLoading] = useState(false);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const router = useRouter();
+
+  if (error) {
+    return (
+      <VStack>
+        <Heading>Something went wrong...</Heading>
+        <Heading>{error?.message}</Heading>
+      </VStack>
+    );
+  }
+
+  if (loadingAuth) {
+    return (
+      <VStack>
+        <Heading>Loading...</Heading>
+      </VStack>
+    );
+  }
+
+  if (!user) {
+    return (
+      <VStack>
+        <Heading>Sign in to create a quiz</Heading>
+        <Button onClick={signIn}>Sign In with Google</Button>
+      </VStack>
+    );
+  }
+
+  return (
+    <>
+      <VStack>
+        <Heading>CreateQuizPage</Heading>
+        <Button onClick={signOut} variant="outline">
+          Sign Out
+        </Button>
+        <Input
+          onChange={(e) => setTitle(e.target.value)}
+          disabled={loading}
+          placeholder="Title"
+        ></Input>
+        <Textarea
+          onChange={(e) => setDescription(e.target.value)}
+          disabled={loading}
+          placeholder="Description"
+          resize="none"
+          h="sm"
+        ></Textarea>
+        <Button onClick={createRoom} isLoading={loading} width="full">
+          Next
+        </Button>
+      </VStack>
+    </>
+  );
+
+  async function createRoom() {
+    setLoading(true);
+
+    const roomsRef = collection(db, "rooms");
+    const pin = await decidePin();
+    const room: Room = {
+      title,
+      description,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30),
+      createdBy: user!.uid,
+      teams: [],
+      pin: pin!,
+      status: "waiting",
+      quizzes: [],
+    };
+
+    try {
+      const docRef = await addDoc(roomsRef, room);
+      router.push(`/new/${docRef.id}`);
+      setTitle("");
+      setDescription("");
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  async function decidePin() {
+    const pin = Math.floor(Math.random() * 10000)
+      .toString()
+      .padStart(4, "0");
+    const roomsRef = collection(db, "rooms");
+    const q = query(
+      roomsRef,
+      where("pin", "==", pin),
+      where("status", "==", "waiting")
+    );
+    const matchedRooms = await getDocs(q);
+
+    if (matchedRooms.size > 0) {
+      // Already exists, try again
+      decidePin();
+    } else {
+      return pin;
+    }
+  }
+}
