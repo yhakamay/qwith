bbd06f5beeaa4f5c227bb2bd97c6632c9ab48d27
Yusuke Hakamaya <yhakamay@student.42tokyo.jp>
2022-11-27 17:47:15 +0900

    add four fields to Room type
    createdAt, updatedAt, expiresAt, and createdBy

--- types/room.ts
+++ types/room.ts
@@ -13,6 +13,10 @@ export type Room = {
   status: "waiting" | "playing" | "closed";
   title: string;
   description?: string;
+  createdAt: Date;
+  updatedAt: Date;
+  expiresAt: Date;
+  createdBy: string;
   quizzes: ChoiceQuiz[];
   teams: Team[];
 };
@@ -24,6 +28,10 @@ export const roomConverter: FirestoreDataConverter<Room> = {
       status: room.status,
       title: room.title,
       description: room.description,
+      createdAt: room.createdAt,
+      updatedAt: room.updatedAt,
+      expiresAt: room.expiresAt,
+      createdBy: room.createdBy,
       quizzes: room.quizzes,
       teams: room.teams,
     };
@@ -38,6 +46,10 @@ export const roomConverter: FirestoreDataConverter<Room> = {
       status: data.status,
       title: data.title,
       description: data.description,
+      createdAt: data.createdAt.toDate(),
+      updatedAt: data.updatedAt.toDate(),
+      expiresAt: data.expiresAt.toDate(),
+      createdBy: data.createdBy,
       quizzes: data.quizzes,
       teams: data.teams,
     };
