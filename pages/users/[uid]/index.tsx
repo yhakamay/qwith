e7394c4a21af9effeef730db47f3e2bdc58f197d
Yusuke Hakamaya <yhakamay@student.42tokyo.jp>
2022-11-27 18:53:12 +0900

    add 'My Rooms page

---
+++ pages/users/[uid]/index.tsx
@@ -0,0 +1,15 @@
+import { VStack, Heading, Text } from "@chakra-ui/react";
+
+export default function MyRoomsPage() {
+  return (
+    <>
+      <VStack>
+        <Heading>My Rooms</Heading>
+        <Text>
+          This page is under construction since this feature depends on Firebase
+          Cloud Functions
+        </Text>
+      </VStack>
+    </>
+  );
+}
