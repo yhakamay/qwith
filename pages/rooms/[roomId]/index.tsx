328b296300d1cdde1c9ce348343fae180362bbe4
Yusuke Hakamaya <yhakamay@student.42tokyo.jp>
2022-11-29 09:24:12 +0900

    Implement QuizPage

--- pages/rooms/[roomId]/index.tsx
+++ pages/rooms/[roomId]/index.tsx
@@ -1,6 +1,12 @@
-import { Center, Spinner } from "@chakra-ui/react";
+import { Center, Heading, Spinner, Text, VStack } from "@chakra-ui/react";
+import { collection, doc } from "firebase/firestore";
 import { useRouter } from "next/router";
-import QuizGate from "../../../components/organisms/quizGate";
+import { useDocumentData } from "react-firebase-hooks/firestore";
+import QuizEntry from "../../../components/organisms/quizEntry";
+import QuizPlay from "../../../components/organisms/quizPlay";
+import QuizWaiting from "../../../components/organisms/quizWaiting";
+import { db } from "../../../firebaseConfig";
+import { roomConverter, Room } from "../../../types/room";
 
 export default function QuizPage() {
   const router = useRouter();
@@ -8,11 +14,63 @@ export default function QuizPage() {
 
   if (!router.isReady) {
     return (
-      <Center>
+      <Center h="100vh">
         <Spinner />
       </Center>
     );
   }
 
-  return <QuizGate roomId={roomId as string} teamId={teamId as string} />;
+  return (
+    <QuizPortal
+      roomId={roomId as string}
+      teamId={teamId as string | undefined}
+    />
+  );
+}
+
+type QuizPortalProps = {
+  roomId: string;
+  teamId?: string;
+};
+
+function QuizPortal(props: QuizPortalProps) {
+  const { roomId, teamId } = props;
+  const roomsRef = collection(db, "rooms");
+  const roomRef = doc(roomsRef, roomId).withConverter(roomConverter);
+  const [room, loading, error, snapshot] = useDocumentData<Room>(roomRef);
+
+  if (error) {
+    return (
+      <VStack h="100vh">
+        <Heading>Something went wrong...</Heading>
+        <Text>{error?.message}</Text>
+      </VStack>
+    );
+  }
+
+  if (!snapshot?.exists() || loading) {
+    return (
+      <Center h="100vh">
+        <Spinner />
+      </Center>
+    );
+  }
+
+  if (room?.status === "closed") {
+    return (
+      <VStack h="100vh">
+        <Text>This room is closed 👋</Text>
+      </VStack>
+    );
+  }
+
+  if (!teamId) {
+    return <QuizEntry roomId={roomId as string} />;
+  }
+
+  if (room?.status === "waiting") {
+    return <QuizWaiting roomId={roomId as string} teamId={teamId as string} />;
+  }
+
+  return <QuizPlay roomId={roomId as string} teamId={teamId as string} />;
 }
