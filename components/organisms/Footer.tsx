b19f06c88ebc99b9dba04c12b5655c20683fb765
Yusuke Hakamaya <yhakamay@student.42tokyo.jp>
2022-12-03 23:08:05 +0900

    Add link to icons on the Footer

--- components/organisms/Footer.tsx
+++ components/organisms/Footer.tsx
@@ -1,35 +1,48 @@
 import { AddIcon } from "@chakra-ui/icons";
 import { Center, HStack, Text } from "@chakra-ui/react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Footer() {
   return (
     <Center my={16}>
       <HStack spacing={4}>
-        <Image
-          src={"/logos/firebase.svg"}
-          alt={"Firebase logo"}
-          width={20}
-          height={20}
-        />
+        <Link
+          href="https://firebase.google.com/"
+          target="_blank"
+          rel="noopener"
+        >
+          <Image
+            src={"/logos/firebase.svg"}
+            alt={"Firebase logo"}
+            width={20}
+            height={20}
+          />
+        </Link>
         <AddIcon fontSize="xs" opacity={0.8} />
-        <Image
-          src={"/logos/nextjs.svg"}
-          alt={"Next.js logo"}
-          width={28}
-          height={28}
-        />
+        <Link href="https://nextjs.org/" target="_blank" rel="noopener">
+          <Image
+            src={"/logos/nextjs.svg"}
+            alt={"Next.js logo"}
+            width={28}
+            height={28}
+          />
+        </Link>
         <Text fontSize="xs" opacity={0.8}>
           by
         </Text>
-        <div style={{ borderRadius: "5px", overflow: "hidden" }}>
+        <Link
+          href="https://twitter.com/ryota_murakami"
+          target="_blank"
+          rel="noopener"
+        >
           <Image
             src="/logos/yhakamaya.png"
             alt={"yhakamaya"}
             width={24}
             height={24}
           />
-        </div>
+        </Link>
       </HStack>
     </Center>
   );
