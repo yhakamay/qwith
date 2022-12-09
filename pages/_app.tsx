f2734c32af501c47f5054f7d1d735a7cf76c3aa0
Yusuke Hakamaya <yhakamay@student.42tokyo.jp>
2022-12-09 23:32:27 +0900

    Add title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,21 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import StandardTemplate from "../components/templates/StandardTemplate";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <StandardTemplate>
-        <Component {...pageProps} />
-      </StandardTemplate>
-    </ChakraProvider>
+    <>
+      <Head>
+        <title>Qwith</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="og:title" content="Qwith" key="title" />
+      </Head>
+      <ChakraProvider>
+        <StandardTemplate>
+          <Component {...pageProps} />
+        </StandardTemplate>
+      </ChakraProvider>
+    </>
   );
 }
